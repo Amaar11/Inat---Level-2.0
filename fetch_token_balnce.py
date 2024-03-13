cbe73a496597f6d4c52d57b3e7630c5b9f02e7a5
Amaar11 <101276120+Amaar11@users.noreply.github.com>
2024-03-13 22:28:29 +0100

    Update fetch_token_balnce.py

--- fetch_token_balnce.py
+++ fetch_token_balnce.py
@@ -1,4 +1,3 @@
-import requests
 from web3 import Web3, HTTPProvider
 from web3.exceptions import ABIFunctionNotFound
 from abi import get_token_abi, addresses, tokens
@@ -6,12 +5,16 @@ from abi import get_token_abi, addresses, tokens
 # Povezivanje sa Ethereum blockchain
 w3 = Web3(HTTPProvider("https://rpc.eth.gateway.fm"))
 
-def balance(address, token_address):
+def balance(address, token_address, token_name):
     try:
-        # Dohvati ABI podatke za trenutni token
+        # Dohvati ABI podatke 
         token_abi = get_token_abi(token_address)
+        if token_abi is None:
+            raise Exception("ABI data not found for token")
+
         token_contract = w3.eth.contract(address=token_address, abi=token_abi)
-        # Dohvatite balans tokena za trenutni račun
+
+        # Dohvati balans tokena za trenutni račun
         token_balance = token_contract.functions.balanceOf(address).call()
         return token_balance
     except ABIFunctionNotFound:
@@ -21,9 +24,7 @@ def balance(address, token_address):
 for address_type, address in addresses:
     print(f"Balans tokena za {address_type}: {address}")
     for token_name, token_address in tokens.items():
-        token_balance = balance(address, token_address)
+        token_balance = balance(address, token_address, token_name)
         if token_balance is not None:
             print(f"{token_name}: {token_balance}")
     print()
-
-
